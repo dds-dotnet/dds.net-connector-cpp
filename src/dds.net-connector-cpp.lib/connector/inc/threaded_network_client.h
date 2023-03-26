157765b6a56395d1d2c8e68d0db2250a7a04577f
usama <mahmoodify@gmail.com>
2023-03-26 22:30:06 +0500

    adding placeholdeer for threaded network client

---
+++ src/dds.net-connector-cpp.lib/connector/inc/threaded_network_client.h
@@ -0,0 +1,18 @@
+#ifndef DDS_DOT_NET_CONNECTOR_INC_THREADED_NETWORK_CLIENT_H_
+#define DDS_DOT_NET_CONNECTOR_INC_THREADED_NETWORK_CLIENT_H_
+
+
+namespace dds {
+  namespace net {
+    namespace connector {
+
+      class ThreadedNetworkClient {
+      };
+
+    }
+  }
+}
+
+
+
+#endif
