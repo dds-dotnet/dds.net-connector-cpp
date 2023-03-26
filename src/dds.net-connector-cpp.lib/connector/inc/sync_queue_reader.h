555ed2369952099581384ebf4afc7e3330459eb9
usama <mahmoodify@gmail.com>
2023-03-26 23:02:25 +0500

    adding types for queue reader and writer

---
+++ src/dds.net-connector-cpp.lib/connector/inc/sync_queue_reader.h
@@ -0,0 +1,19 @@
+#ifndef DDS_DOT_NET_CONNECTOR_INC_SYNC_QUEUE_READER_H_
+#define DDS_DOT_NET_CONNECTOR_INC_SYNC_QUEUE_READER_H_
+
+
+namespace dds {
+  namespace net {
+    namespace connector {
+
+      [template class T]
+      class SyncQueueReader {
+      };
+
+    }
+  }
+}
+
+
+
+#endif
---
+++ src/dds.net-connector-cpp.lib/connector/inc/sync_queue_writer.h
@@ -0,0 +1,19 @@
+#ifndef DDS_DOT_NET_CONNECTOR_INC_SYNC_QUEUE_WRITER_H_
+#define DDS_DOT_NET_CONNECTOR_INC_SYNC_QUEUE_WRITER_H_
+
+
+namespace dds {
+  namespace net {
+    namespace connector {
+
+      [template class T]
+      class SyncQueueWriter {
+      };
+
+    }
+  }
+}
+
+
+
+#endif
