f40572c96c799053575bbb0e06a7215b576f38cc
usama <mahmoodify@gmail.com>
2023-03-26 22:25:59 +0500

    adding logger iface

---
+++ src/dds.net-connector-cpp.lib/connector/inc/logger.h
@@ -0,0 +1,28 @@
+#ifndef DDS_DOT_NET_CONNECTOR_INC_LOGGER_H_
+#define DDS_DOT_NET_CONNECTOR_INC_LOGGER_H_
+
+
+namespace dds {
+  namespace net {
+    namespace connector {
+
+      class Logger {
+      public:
+        enum LogLevel {
+          LOG_LEVEL_INFORMATION,
+          LOG_LEVEL_WARNING,
+          LOG_LEVEL_ERROR
+        };
+
+        virtual void logInformation(const char* messageLine) = 0;
+        virtual void logWarning(const char* messageLine) = 0;
+        virtual void logError(const char* messageLine) = 0;
+      };
+
+    }
+  }
+}
+
+
+
+#endif
---
+++ src/dds.net-connector-cpp.lib/connector/src/logger.cpp
@@ -0,0 +1,3 @@
+#include "inc/logger.h"
+
+
