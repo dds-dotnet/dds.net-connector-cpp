1155c6761e9e4bea4019d01a9d1b7ebb42f18b07
usama <mahmoodify@gmail.com>
2023-03-26 23:06:01 +0500

    adding types for packet from / to server

---
+++ src/dds.net-connector-cpp.lib/connector/inc/packet_to_server.h
@@ -0,0 +1,18 @@
+#ifndef DDS_DOT_NET_CONNECTOR_INC_PACKET_TO_SERVER_H_
+#define DDS_DOT_NET_CONNECTOR_INC_PACKET_TO_SERVER_H_
+
+
+namespace dds {
+  namespace net {
+    namespace connector {
+
+      class PacketToServer {
+      };
+
+    }
+  }
+}
+
+
+
+#endif
