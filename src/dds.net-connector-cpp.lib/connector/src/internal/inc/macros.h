a7062b47639cab8f050a9452d93847ef0bc1022e
usama <mahmoodify@gmail.com>
2023-03-27 21:42:24 +0500

    adding macros

---
+++ src/dds.net-connector-cpp.lib/connector/src/internal/inc/macros.h
@@ -0,0 +1,17 @@
+#ifndef DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_INC_MACROS_H_
+#define DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_INC_MACROS_H_
+
+
+#include <chrono>
+#include <thread>
+
+
+#define SLEEP_MS(ms)     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+
+
+
+
+
+
+#endif
