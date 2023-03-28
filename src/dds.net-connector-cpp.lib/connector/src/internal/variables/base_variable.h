07054ed57551f2881cf063bfac3bb0cc913eed5b
usama <mahmoodify@gmail.com>
2023-03-28 17:44:01 +0500

    renaming the var namespace

--- src/dds.net-connector-cpp.lib/connector/src/internal/variables/base_variable.h
+++ src/dds.net-connector-cpp.lib/connector/src/internal/variables/base_variable.h
@@ -1,12 +1,12 @@
-#ifndef DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_VARIABLE_BASE_VARIABLE_H_
-#define DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_VARIABLE_BASE_VARIABLE_H_
+#ifndef DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_VARIABLES_BASE_VARIABLE_H_
+#define DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_VARIABLES_BASE_VARIABLE_H_
 
 
 namespace dds {
   namespace net {
     namespace connector {
       namespace _internal {
-        namespace variable {
+        namespace variables {
 
           class BaseVariable {
           };
