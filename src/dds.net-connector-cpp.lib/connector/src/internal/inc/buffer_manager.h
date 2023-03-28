5713df2e1bdd4be99472f620e38f95b44b67575f
usama <mahmoodify@gmail.com>
2023-03-29 02:55:53 +0500

    impl the buffer manager

--- src/dds.net-connector-cpp.lib/connector/src/internal/inc/buffer_manager.h
+++ src/dds.net-connector-cpp.lib/connector/src/internal/inc/buffer_manager.h
@@ -1,16 +1,51 @@
 #ifndef DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_INC_BUFFER_MANAGER_H_
 #define DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_INC_BUFFER_MANAGER_H_
 
+#include <list>
+#include <mutex>
+
 
 namespace dds {
   namespace net {
     namespace connector {
       namespace _internal {
 
+        typedef struct {
+          void* address;
+          int size;
+          bool isFree;
+        } BufferDefinition;
+
         class BufferManager {
         public:
 
           BufferManager();
+
+          void* get1k();
+          void* get2k();
+          void* get4k();
+          void* get8k();
+          void* get16k();
+          void* get32k();
+          void* get64k();
+          void* get128k();
+          void* get256k();
+          void* get512k();
+          void* get1M();
+          void* get2M();
+          void* get4M();
+          void* get8M();
+          void* get16M();
+          void* get32M();
+          void* get64M();
+
+          void* get(int size);
+          void free(void* freeBuffer);
+
+
+        private:
+          std::mutex buffLock;
+          std::list<BufferDefinition> buff;
         };
 
       }
--- src/dds.net-connector-cpp.lib/connector/src/internal/src/buffer_manager.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/buffer_manager.cpp
@@ -1,5 +1,147 @@
 #include "src/internal/inc/buffer_manager.h"
 
+#include <memory.h>
+
+#define BYTES_IN_1K   1024
+#define BYTES_IN_1M   BYTES_IN_1K * BYTES_IN_1K
+
+
 dds::net::connector::_internal::BufferManager::BufferManager()
 {
+}
+
+void* dds::net::connector::_internal::BufferManager::get1k()
+{
+  return get(BYTES_IN_1K * 1);
+}
+
+void* dds::net::connector::_internal::BufferManager::get2k()
+{
+  return get(BYTES_IN_1K * 2);
+}
+
+void* dds::net::connector::_internal::BufferManager::get4k()
+{
+  return get(BYTES_IN_1K * 4);
+}
+
+void* dds::net::connector::_internal::BufferManager::get8k()
+{
+  return get(BYTES_IN_1K * 8);
+}
+
+void* dds::net::connector::_internal::BufferManager::get16k()
+{
+  return get(BYTES_IN_1K * 16);
+}
+
+void* dds::net::connector::_internal::BufferManager::get32k()
+{
+  return get(BYTES_IN_1K * 32);
+}
+
+void* dds::net::connector::_internal::BufferManager::get64k()
+{
+  return get(BYTES_IN_1K * 64);
+}
+
+void* dds::net::connector::_internal::BufferManager::get128k()
+{
+  return get(BYTES_IN_1K * 128);
+}
+
+void* dds::net::connector::_internal::BufferManager::get256k()
+{
+  return get(BYTES_IN_1K * 256);
+}
+
+void* dds::net::connector::_internal::BufferManager::get512k()
+{
+  return get(BYTES_IN_1K * 512);
+}
+
+void* dds::net::connector::_internal::BufferManager::get1M()
+{
+  return get(BYTES_IN_1M * 1);
+}
+
+void* dds::net::connector::_internal::BufferManager::get2M()
+{
+  return get(BYTES_IN_1M * 2);
+}
+
+void* dds::net::connector::_internal::BufferManager::get4M()
+{
+  return get(BYTES_IN_1M * 4);
+}
+
+void* dds::net::connector::_internal::BufferManager::get8M()
+{
+  return get(BYTES_IN_1M * 8);
+}
+
+void* dds::net::connector::_internal::BufferManager::get16M()
+{
+  return get(BYTES_IN_1M * 16);
+}
+
+void* dds::net::connector::_internal::BufferManager::get32M()
+{
+  return get(BYTES_IN_1M * 32);
+}
+
+void* dds::net::connector::_internal::BufferManager::get64M()
+{
+  return get(BYTES_IN_1M * 64);
+}
+
+void* dds::net::connector::_internal::BufferManager::get(int size)
+{
+  buffLock.lock();
+
+  void* ret = nullptr;
+
+  for (BufferDefinition& buff : buff)
+  {
+    if (buff.size == size &&
+        buff.isFree == true)
+    {
+      buff.isFree = false;
+      ret = buff.address;
+      break;
+    }
+  }
+
+  if (ret == nullptr)
+  {
+    BufferDefinition newBuffer;
+
+    newBuffer.address = malloc(size);
+    newBuffer.size = size;
+    newBuffer.isFree = false;
+
+    ret = newBuffer.address;
+
+    buff.push_back(newBuffer);
+  }
+
+  buffLock.unlock();
+
+  return ret;
+}
+
+void dds::net::connector::_internal::BufferManager::free(void* freeBuffer)
+{
+  buffLock.lock();
+
+  for (BufferDefinition& buff : buff)
+  {
+    if (buff.address == freeBuffer)
+    {
+      buff.isFree = true;
+      break;
+    }
+  }
+
+  buffLock.unlock();
 }
