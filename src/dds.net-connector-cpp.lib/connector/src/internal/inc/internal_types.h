c282a1ce2fc5dc730643d9d4cd270309997235b5
usama <mahmoodify@gmail.com>
2023-03-28 20:00:45 +0500

    prepending the enums

--- src/dds.net-connector-cpp.lib/connector/src/internal/inc/internal_types.h
+++ src/dds.net-connector-cpp.lib/connector/src/internal/inc/internal_types.h
@@ -5,39 +5,36 @@
 
 
 typedef enum {
-  HANDSHAKE = 0,                      // Initialization information / configuration exchange.
-  VARIABLES_REGISTRATION = 1,         // Registering variables with the server.
-  VARIABLES_UPDATE_AT_SERVER = 2,     // Updating variable values at the server.
-  VARIABLES_UPDATE_FROM_SERVER = 3,   // Updating variable values at the client.
-  ERROR_RESPONSE_FROM_SERVER = 4,     // Error response from the server.
-
-  UNKNOWN_PACKET // Unknown packet.
+  PACKET_ID_HANDSHAKE = 0,                      // Initialization information / configuration exchange.
+  PACKET_ID_VARIABLES_REGISTRATION = 1,         // Registering variables with the server.
+  PACKET_ID_VARIABLES_UPDATE_AT_SERVER = 2,     // Updating variable values at the server.
+  PACKET_ID_VARIABLES_UPDATE_FROM_SERVER = 3,   // Updating variable values at the client.
+  PACKET_ID_ERROR_RESPONSE_FROM_SERVER = 4,     // Error response from the server.
+  PACKET_ID_UNKNOWN                             // Unknown packet.
 } PacketId;
 
 
 typedef enum {
-  PRIMITIVE = 0,  // Represents the very basic variable type, e.g., string, integer, float, etc.
-  RAW_BYTES = 1,  // Represents a sequence of bytes (unsigned bytes).
-
-  UNKNOWN_VARIABLE_TYPE // Unknown type.
+  VARIABLE_TYPE_PRIMITIVE = 0,  // Represents the very basic variable type, e.g., string, integer, float, etc.
+  VARIABLE_TYPE_RAW_BYTES = 1,  // Represents a sequence of bytes (unsigned bytes).
+  VARIABLE_TYPE_UNKNOWN         // Unknown type.
 } VariableType;
 
 
 typedef enum {
-  STRING = 0,         // Represents a string of characters.
-  BOOLEAN = 1,        // Represents a boolean (True or False).
-  BYTE = 2,           // Represents a 1-byte Signed Integer.
-  WORD = 3,           // Represents a 2-byte Signed Integer.
-  DWORD = 4,          // Represents a 4-byte Signed Integer.
-  QWORD = 5,          // Represents an 8-byte Signed Integer.
-  UNSIGNED_BYTE = 6,  // Represents a 1-byte Unsigned Integer.
-  UNSIGNED_WORD = 7,  // Represents a 2-byte Unsigned Integer.
-  UNSIGNED_DWORD = 8, // Represents a 4-byte Unsigned Integer.
-  UNSIGNED_QWORD = 9, // Represents an 8-byte Unsigned Integer.
-  SINGLE = 10,        // Represents a single precision 4-byte Floating-point number.
-  DOUBLE = 11,        // Represents a double precision 8-byte Floating-point number.
-
-  UNKNOWN_PRIMITIVE_TYPE // Unknown type.
+  PRIMITIVE_TYPE_STRING = 0,           // Represents a string of characters.
+  PRIMITIVE_TYPE_BOOLEAN = 1,          // Represents a boolean (True or False).
+  PRIMITIVE_TYPE_BYTE = 2,             // Represents a 1-byte Signed Integer.
+  PRIMITIVE_TYPE_WORD = 3,             // Represents a 2-byte Signed Integer.
+  PRIMITIVE_TYPE_DWORD = 4,            // Represents a 4-byte Signed Integer.
+  PRIMITIVE_TYPE_QWORD = 5,            // Represents an 8-byte Signed Integer.
+  PRIMITIVE_TYPE_UNSIGNED_BYTE = 6,    // Represents a 1-byte Unsigned Integer.
+  PRIMITIVE_TYPE_UNSIGNED_WORD = 7,    // Represents a 2-byte Unsigned Integer.
+  PRIMITIVE_TYPE_UNSIGNED_DWORD = 8,   // Represents a 4-byte Unsigned Integer.
+  PRIMITIVE_TYPE_UNSIGNED_QWORD = 9,   // Represents an 8-byte Unsigned Integer.
+  PRIMITIVE_TYPE_SINGLE = 10,          // Represents a single precision 4-byte Floating-point number.
+  PRIMITIVE_TYPE_DOUBLE = 11,          // Represents a double precision 8-byte Floating-point number.
+  PRIMITIVE_TYPE_UNKNOWN               // Unknown type.
 } PrimitiveType;
 
 
