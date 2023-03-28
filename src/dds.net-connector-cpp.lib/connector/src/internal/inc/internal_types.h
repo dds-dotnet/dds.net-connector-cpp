c6f539641ea20b1f1396add1f472da9cd27b7bbb
usama <mahmoodify@gmail.com>
2023-03-28 07:03:50 +0500

    adding primitive type

--- src/dds.net-connector-cpp.lib/connector/src/internal/inc/internal_types.h
+++ src/dds.net-connector-cpp.lib/connector/src/internal/inc/internal_types.h
@@ -12,12 +12,31 @@ typedef enum {
   UnknownPacket // Unknown packet.
 } PacketId;
 
+
 typedef enum {
-  Primitive, // Represents the very basic variable type, e.g., string, integer, float, etc.
-  RawBytes,  // Represents a sequence of bytes (unsigned bytes).
+  Primitive = 0, // Represents the very basic variable type, e.g., string, integer, float, etc.
+  RawBytes = 1,  // Represents a sequence of bytes (unsigned bytes).
 
   UnknownVariableType // Unknown type.
 } VariableType;
+
+
+typedef enum {
+  String = 0,        // Represents a string of characters.
+  Boolean = 1,       // Represents a boolean (True or False).
+  Byte = 2,          // Represents a 1-byte Signed Integer.
+  Word = 3,          // Represents a 2-byte Signed Integer.
+  DWord = 4,         // Represents a 4-byte Signed Integer.
+  QWord = 5,         // Represents an 8-byte Signed Integer.
+  UnsignedByte = 6,  // Represents a 1-byte Unsigned Integer.
+  UnsignedWord = 7,  // Represents a 2-byte Unsigned Integer.
+  UnsignedDWord = 8, // Represents a 4-byte Unsigned Integer.
+  UnsignedQWord = 9, // Represents an 8-byte Unsigned Integer.
+  Single = 10,       // Represents a single precision 4-byte Floating-point number.
+  Double = 11,       // Represents a double precision 8-byte Floating-point number.
+
+  UnknownPrimitiveType // Unknown type.
+} PrimitiveType;
 
 
 
