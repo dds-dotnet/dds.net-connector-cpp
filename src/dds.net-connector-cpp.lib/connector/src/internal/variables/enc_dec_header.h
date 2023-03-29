0c8f8fb16d76bf3d9cd94a58d8ca566d285f4b8f
usama <mahmoodify@gmail.com>
2023-03-30 04:20:30 +0500

    outlining the methods for writing data

--- src/dds.net-connector-cpp.lib/connector/src/internal/variables/enc_dec_header.h
+++ src/dds.net-connector-cpp.lib/connector/src/internal/variables/enc_dec_header.h
@@ -1,6 +1,8 @@
 #ifndef DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_VARIABLES_ENC_DEC_HEADER_H_
 #define DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_VARIABLES_ENC_DEC_HEADER_H_
 
+#include "src/internal/inc/internal_types.h"
+
 
 namespace dds {
   namespace net {
@@ -13,6 +15,20 @@ namespace dds {
           public:
             static const int ID_SIZE_ON_BUFFER = sizeof(short);
             static const int VARIABLE_TYPE_SIZE_ON_BUFFER = 2;
+
+            static void writeId(BufferAddress buffer, int& offset, int id)
+            {
+              //- 
+              //- TODO: Impl
+              //- 
+            }
+
+            static void writeVariableType(BufferAddress buffer, int& offset, VariableType variableType)
+            {
+              //- 
+              //- TODO: Impl
+              //- 
+            }
           };
         }
       }
