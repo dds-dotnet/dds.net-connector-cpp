c6a4e7d705f699b010709606fabf684384a53fd2
usama <mahmoodify@gmail.com>
2023-03-29 06:15:37 +0500

    impl ip address checking and blanks stripping

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/string_helper.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/string_helper.cpp
@@ -3,12 +3,14 @@
 #include <regex>
 
 
+static std::regex ipv4("\\s*(\\d{1,3})\\s*\\.\\s*(\\d{1,3})\\s*\\.\\s*(\\d{1,3})\\s*\\.\\s*(\\d{1,3})\\s*");
+static std::regex spaces("\\s+");
+
+
 bool dds::net::connector::_internal::StringHelper::isValidIPv4Address(std::string& address)
 {
-  return false;
+  return std::regex_match(address, ipv4);
 }
-
-static std::regex spaces("\\s+");
 
 std::string dds::net::connector::_internal::StringHelper::removeSpaces(std::string& inp)
 {
