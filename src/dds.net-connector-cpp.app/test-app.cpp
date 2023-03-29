f390408cb04b05f5f32623fe8822ddf3cbd173ca
usama <mahmoodify@gmail.com>
2023-03-29 07:25:44 +0500

    adding outline for connector init

--- src/dds.net-connector-cpp.app/test-app.cpp
+++ src/dds.net-connector-cpp.app/test-app.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+
 #include "ddsconnector.h"
+#include "logger.h"
+
+using namespace std;
+using namespace dds::net::connector;
+
 
 int main()
 {
     std::cout << "DDS.Net Connected App!\n";
+
+    string appName = "My CPP App";
+    string serverIP = "127.0.0.1";
+    unsigned short serverPort = 44556;
+
+    DdsConnector* connector = new DdsConnector(appName, serverIP, serverPort, new ConsoleLogger());
 }
