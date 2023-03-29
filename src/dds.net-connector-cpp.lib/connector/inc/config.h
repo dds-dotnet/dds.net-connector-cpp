8c50aa23649f2a2fd443c85cc17278ac5549f297
usama <mahmoodify@gmail.com>
2023-03-29 23:18:03 +0500

    adding target platform for platform dependant code

--- src/dds.net-connector-cpp.lib/connector/inc/config.h
+++ src/dds.net-connector-cpp.lib/connector/inc/config.h
@@ -5,6 +5,14 @@
 #define LIBRARY_VERSION            "1.0.0"
 #define BASE_TIME_SLOT_MSEC        50
 
+#define PLATFORM_GNU_LINUX              0x00010000
+#define PLATFORM_MAC_OS                 0x00020000
+#define PLATFORM_WINDOWS                0x00040000
+
+#ifndef TARGET_PLATFORM
+  #define   TARGET_PLATFORM        PLATFORM_GNU_LINUX
+#endif
+
 
 
 #endif
