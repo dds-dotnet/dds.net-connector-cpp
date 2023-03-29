70fe507921369c813c976009193f1b5ce0fb6b41
usama <mahmoodify@gmail.com>
2023-03-29 06:10:52 +0500

    impl spaces removal

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/string_helper.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/string_helper.cpp
@@ -1,11 +1,16 @@
 #include "src/internal/inc/string_helper.h"
 
+#include <regex>
+
+
 bool dds::net::connector::_internal::StringHelper::isValidIPv4Address(std::string& address)
 {
   return false;
 }
 
+static std::regex spaces("\\s+");
+
 std::string dds::net::connector::_internal::StringHelper::removeSpaces(std::string& inp)
 {
-  return inp;
+  return std::regex_replace(inp, spaces, "");
 }
