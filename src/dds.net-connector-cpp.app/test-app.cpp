378dd7dad30d38c1540448a6843c4eeccc1210cf
usama <mahmoodify@gmail.com>
2023-03-31 07:15:25 +0500

    reformatting

--- src/dds.net-connector-cpp.app/test-app.cpp
+++ src/dds.net-connector-cpp.app/test-app.cpp
@@ -13,29 +13,29 @@ void wait_for_exit_key();
 
 int main()
 {
-    cout << "DDS.Net Connected App!" << endl;
-    cout << "======================" << endl;
-    cout << "Press Ctrl+C to exit." << endl << endl;
+  string appName = "DDS.Net Connected App";
+  string serverIP = "127.0.0.1";
+  unsigned short serverPort = 44556;
 
-    string appName = "My CPP App";
-    string serverIP = "127.0.0.1";
-    unsigned short serverPort = 44556;
+  cout << appName << endl;
+  cout << "-----" << endl;
+  cout << "Press Ctrl+C to exit." << endl << endl;
 
-    try
-    {
-      DdsConnector* connector =
-        new DdsConnector(
-          appName,
-          serverIP, serverPort, new ConsoleLogger());
-    }
-    catch (exception& ex)
-    {
-      cout << "Exception! " << ex.what() << endl;
-    }
+  try
+  {
+    DdsConnector* connector =
+      new DdsConnector(
+        appName,
+        serverIP, serverPort, new ConsoleLogger());
+  }
+  catch (exception& ex)
+  {
+    cout << "Exception! " << ex.what() << endl;
+  }
 
-    wait_for_exit_key();
+  wait_for_exit_key();
 
-    cout << endl << endl << endl;
+  cout << endl << endl << endl;
 }
 
 void wait_for_exit_key()
@@ -43,3 +43,4 @@ void wait_for_exit_key()
   char ch;
   while ((ch = getc(stdin)) != 122);
 }
+
