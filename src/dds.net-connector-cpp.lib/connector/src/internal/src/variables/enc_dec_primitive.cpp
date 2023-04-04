69b95ff62e79b58cefabe4d762324e1740a8b75f
usama <mahmoodify@gmail.com>
2023-04-04 09:27:37 +0500

    adding u16 converter

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/enc_dec_primitive.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/enc_dec_primitive.cpp
@@ -1,5 +1,11 @@
 #include "src/internal/inc/variables/enc_dec_primitive.h"
 
+#include <codecvt>
+
+
+static std::wstring_convert<std::codecvt<char16_t, char, std::mbstate_t>, char16_t> u16Converter;
+
+
 
 PrimitiveType
   dds::net::connector::_internal::variables::
