cfcfe3ecbd932fa1582f0f2964a512db56c42084
usama <mahmoodify@gmail.com>
2023-04-05 01:15:33 +0500

    including enc/dec for primitives - for ease of usage

--- src/dds.net-connector-cpp.lib/connector/src/internal/inc/variables/base_primitive.h
+++ src/dds.net-connector-cpp.lib/connector/src/internal/inc/variables/base_primitive.h
@@ -2,6 +2,7 @@
 #define DDS_DOT_NET_CONNECTOR_SRC_INTERNAL_INC_VARIABLES_BASE_PRIMITIVE_H_
 
 #include "src/internal/inc/variables/base_variable.h"
+#include "src/internal/inc/variables/enc_dec_primitive.h"
 
 
 namespace dds {
