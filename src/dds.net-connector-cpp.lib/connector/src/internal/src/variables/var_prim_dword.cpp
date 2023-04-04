4508b16ea178a9af5e305d2c4e59dbe0bf55c915
usama <mahmoodify@gmail.com>
2023-04-05 02:25:10 +0500

    impl dword variable handler

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_dword.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_dword.cpp
@@ -1,3 +1,74 @@
 #include "src/internal/inc/variables/var_prim_dword.h"
 
 
+
+dds::net::connector::_internal::variables::
+  DWordVariable::DWordVariable(
+    std::string& name,
+    Periodicity periodicity,
+    DWordProvider dWordProvider,
+    DWordConsumer dWordConsumer)
+
+  : BasePrimitive(name, PRIMITIVE_TYPE_DWORD, periodicity)
+{
+  valueProvider = dWordProvider;
+  valueConsumer = dWordConsumer;
+
+  value = 0;
+}
+
+std::string
+  dds::net::connector::_internal::variables::
+  DWordVariable::getPrintableTypeName()
+{
+  return std::string("DWord");
+}
+
+void
+  dds::net::connector::_internal::variables::
+  DWordVariable::resetValue()
+{
+  value = 0;
+}
+
+bool
+  dds::net::connector::_internal::variables::
+  DWordVariable::refreshValue()
+{
+  if (valueProvider != nullptr)
+  {
+    int newValue = valueProvider(name);
+
+    if (value != newValue)
+    {
+      value = newValue;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  DWordVariable::invokeValueAwaiter()
+{
+  if (valueConsumer != nullptr)
+  {
+    valueConsumer(name, value);
+  }
+}
+
+int
+  dds::net::connector::_internal::variables::
+  DWordVariable::getValueSizeOnBuffer()
+{
+  return 4;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  DWordVariable::writeValueOnBuffer(BufferAddress buffer, int& offset)
+{
+  EncDecPrimitive::writeDWord(buffer, offset, value);
+}
