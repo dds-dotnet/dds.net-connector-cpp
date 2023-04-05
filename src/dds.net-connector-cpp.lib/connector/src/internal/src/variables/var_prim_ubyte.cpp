b6eba573378a7df6874bba7c4ca8e74be0640e4d
usama <mahmoodify@gmail.com>
2023-04-05 17:55:25 +0500

    impl unsigned byte handler

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_ubyte.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_ubyte.cpp
@@ -1,3 +1,74 @@
 #include "src/internal/inc/variables/var_prim_ubyte.h"
 
 
+
+dds::net::connector::_internal::variables::
+  UnsignedByteVariable::UnsignedByteVariable(
+    std::string& name,
+    Periodicity periodicity,
+    UnsignedByteProvider unsignedByteProvider,
+    UnsignedByteConsumer unsignedByteConsumer)
+
+  : BasePrimitive(name, PRIMITIVE_TYPE_UNSIGNED_BYTE, periodicity)
+{
+  valueProvider = unsignedByteProvider;
+  valueConsumer = unsignedByteConsumer;
+
+  value = 0;
+}
+
+std::string
+  dds::net::connector::_internal::variables::
+  UnsignedByteVariable::getPrintableTypeName()
+{
+  return std::string("Unsigned Byte");
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedByteVariable::resetValue()
+{
+  value = 0;
+}
+
+bool
+  dds::net::connector::_internal::variables::
+  UnsignedByteVariable::refreshValue()
+{
+  if (valueProvider != nullptr)
+  {
+    unsigned char newValue = valueProvider(name);
+
+    if (value != newValue)
+    {
+      value = newValue;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedByteVariable::invokeValueAwaiter()
+{
+  if (valueConsumer != nullptr)
+  {
+    valueConsumer(name, value);
+  }
+}
+
+int
+  dds::net::connector::_internal::variables::
+  UnsignedByteVariable::getValueSizeOnBuffer()
+{
+  return 1;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedByteVariable::writeValueOnBuffer(BufferAddress buffer, int& offset)
+{
+  EncDecPrimitive::writeUnsignedByte(buffer, offset, value);
+}
