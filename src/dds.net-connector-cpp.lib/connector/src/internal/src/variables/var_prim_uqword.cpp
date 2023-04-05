f398612485fd0fa5775c46b4b9a347fa1d21f752
usama <mahmoodify@gmail.com>
2023-04-05 18:05:33 +0500

    impl the unsigned qword handler

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_uqword.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_uqword.cpp
@@ -1,3 +1,74 @@
 #include "src/internal/inc/variables/var_prim_uqword.h"
 
 
+
+dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::UnsignedQWordVariable(
+    std::string& name,
+    Periodicity periodicity,
+    UnsignedQWordProvider unsignedQWordProvider,
+    UnsignedQWordConsumer unsignedQWordConsumer)
+
+  : BasePrimitive(name, PRIMITIVE_TYPE_UNSIGNED_QWORD, periodicity)
+{
+  valueProvider = unsignedQWordProvider;
+  valueConsumer = unsignedQWordConsumer;
+
+  value = 0;
+}
+
+std::string
+  dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::getPrintableTypeName()
+{
+  return std::string("Unsigned QWord");
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::resetValue()
+{
+  value = 0;
+}
+
+bool
+  dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::refreshValue()
+{
+  if (valueProvider != nullptr)
+  {
+    unsigned long long newValue = valueProvider(name);
+
+    if (value != newValue)
+    {
+      value = newValue;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::invokeValueAwaiter()
+{
+  if (valueConsumer != nullptr)
+  {
+    valueConsumer(name, value);
+  }
+}
+
+int
+  dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::getValueSizeOnBuffer()
+{
+  return 8;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedQWordVariable::writeValueOnBuffer(BufferAddress buffer, int& offset)
+{
+  EncDecPrimitive::writeUnsignedQWord(buffer, offset, value);
+}
