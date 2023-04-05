fbc4bc29e5285b5971327c74329a2e939a4e2b6a
usama <mahmoodify@gmail.com>
2023-04-05 17:50:05 +0500

    impl the string handler

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_string.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_string.cpp
@@ -1,3 +1,74 @@
 #include "src/internal/inc/variables/var_prim_string.h"
 
 
+
+dds::net::connector::_internal::variables::
+  StringVariable::StringVariable(
+    std::string& name,
+    Periodicity periodicity,
+    StringProvider stringProvider,
+    StringConsumer stringConsumer)
+
+  : BasePrimitive(name, PRIMITIVE_TYPE_STRING, periodicity)
+{
+  valueProvider = stringProvider;
+  valueConsumer = stringConsumer;
+
+  value = "";
+}
+
+std::string
+  dds::net::connector::_internal::variables::
+  StringVariable::getPrintableTypeName()
+{
+  return std::string("String");
+}
+
+void
+  dds::net::connector::_internal::variables::
+  StringVariable::resetValue()
+{
+  value = "";
+}
+
+bool
+  dds::net::connector::_internal::variables::
+  StringVariable::refreshValue()
+{
+  if (valueProvider != nullptr)
+  {
+    std::string newValue = valueProvider(name);
+
+    if (value != newValue)
+    {
+      value = newValue;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  StringVariable::invokeValueAwaiter()
+{
+  if (valueConsumer != nullptr)
+  {
+    valueConsumer(name, value);
+  }
+}
+
+int
+  dds::net::connector::_internal::variables::
+  StringVariable::getValueSizeOnBuffer()
+{
+  return EncDecPrimitive::getStringSizeOnBuffer(value);
+}
+
+void
+  dds::net::connector::_internal::variables::
+  StringVariable::writeValueOnBuffer(BufferAddress buffer, int& offset)
+{
+  EncDecPrimitive::writeString(buffer, offset, value);
+}
