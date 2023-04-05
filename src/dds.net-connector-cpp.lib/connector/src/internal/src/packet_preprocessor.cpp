7bed296c94e43c3c8747ac325f280b20e2d9c8e3
usama <mahmoodify@gmail.com>
2023-04-06 02:33:05 +0500

    adding placeholder class for packet preprocessing

---
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/packet_preprocessor.cpp
@@ -0,0 +1,14 @@
+#include "src/internal/inc/packet_preprocessor.h"
+
+#include "src/internal/inc/buffer_manager.h"
+
+dds::net::connector::_internal::
+  PacketPreprocessor::PacketPreprocessor(BufferManager* bufferManager)
+{
+  this->bufferManager = bufferManager;
+}
+
+dds::net::connector::_internal::
+  PacketPreprocessor::~PacketPreprocessor()
+{
+}
