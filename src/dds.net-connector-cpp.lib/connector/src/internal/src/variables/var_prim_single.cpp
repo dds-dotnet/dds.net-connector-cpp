109ab1470a33def43e39dd4e2f3e8d6d4be018bc
usama <mahmoodify@gmail.com>
2023-04-05 17:42:51 +0500

    impl single variable handler

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_single.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_single.cpp
@@ -1,3 +1,74 @@
 #include "src/internal/inc/variables/var_prim_single.h"
 
 
+
+dds::net::connector::_internal::variables::
+  SingleVariable::SingleVariable(
+    std::string& name,
+    Periodicity periodicity,
+    SingleProvider singleProvider,
+    SingleConsumer singleConsumer)
+
+  : BasePrimitive(name, PRIMITIVE_TYPE_SINGLE, periodicity)
+{
+  valueProvider = singleProvider;
+  valueConsumer = singleConsumer;
+
+  value = 0.0f;
+}
+
+std::string
+  dds::net::connector::_internal::variables::
+  SingleVariable::getPrintableTypeName()
+{
+  return std::string("Single");
+}
+
+void
+  dds::net::connector::_internal::variables::
+  SingleVariable::resetValue()
+{
+  value = 0.0f;
+}
+
+bool
+  dds::net::connector::_internal::variables::
+  SingleVariable::refreshValue()
+{
+  if (valueProvider != nullptr)
+  {
+    float newValue = valueProvider(name);
+
+    if (value != newValue)
+    {
+      value = newValue;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  SingleVariable::invokeValueAwaiter()
+{
+  if (valueConsumer != nullptr)
+  {
+    valueConsumer(name, value);
+  }
+}
+
+int
+  dds::net::connector::_internal::variables::
+  SingleVariable::getValueSizeOnBuffer()
+{
+  return 4;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  SingleVariable::writeValueOnBuffer(BufferAddress buffer, int& offset)
+{
+  EncDecPrimitive::writeSingle(buffer, offset, value);
+}
