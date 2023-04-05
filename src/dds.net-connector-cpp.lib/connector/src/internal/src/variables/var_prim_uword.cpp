baffd58f2849b3a990b51000d4a081f9b87216a6
usama <mahmoodify@gmail.com>
2023-04-05 18:10:40 +0500

    impl the unsigned word handler

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_uword.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/variables/var_prim_uword.cpp
@@ -1,3 +1,74 @@
 #include "src/internal/inc/variables/var_prim_uword.h"
 
 
+
+dds::net::connector::_internal::variables::
+  UnsignedWordVariable::UnsignedWordVariable(
+    std::string& name,
+    Periodicity periodicity,
+    UnsignedWordProvider unsignedWordProvider,
+    UnsignedWordConsumer unsignedWordConsumer)
+
+  : BasePrimitive(name, PRIMITIVE_TYPE_UNSIGNED_WORD, periodicity)
+{
+  valueProvider = unsignedWordProvider;
+  valueConsumer = unsignedWordConsumer;
+
+  value = 0;
+}
+
+std::string
+  dds::net::connector::_internal::variables::
+  UnsignedWordVariable::getPrintableTypeName()
+{
+  return std::string("Unsigned Word");
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedWordVariable::resetValue()
+{
+  value = 0;
+}
+
+bool
+  dds::net::connector::_internal::variables::
+  UnsignedWordVariable::refreshValue()
+{
+  if (valueProvider != nullptr)
+  {
+    unsigned short newValue = valueProvider(name);
+
+    if (value != newValue)
+    {
+      value = newValue;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedWordVariable::invokeValueAwaiter()
+{
+  if (valueConsumer != nullptr)
+  {
+    valueConsumer(name, value);
+  }
+}
+
+int
+  dds::net::connector::_internal::variables::
+  UnsignedWordVariable::getValueSizeOnBuffer()
+{
+  return 2;
+}
+
+void
+  dds::net::connector::_internal::variables::
+  UnsignedWordVariable::writeValueOnBuffer(BufferAddress buffer, int& offset)
+{
+  EncDecPrimitive::writeUnsignedWord(buffer, offset, value);
+}
