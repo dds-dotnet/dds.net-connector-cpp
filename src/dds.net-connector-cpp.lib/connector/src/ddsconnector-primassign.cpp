3df73e7a65ff4676b9e77dd4980575a65c1b082a
usama <mahmoodify@gmail.com>
2023-04-07 07:51:31 +0500

    impl single type assignment

--- src/dds.net-connector-cpp.lib/connector/src/ddsconnector-primassign.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/ddsconnector-primassign.cpp
@@ -791,6 +791,105 @@ bool
   dds::net::connector::
   DdsConnector::updatePrimitiveVariableWithSingle(BasePrimitive* bpv, float v)
 {
+  if (bpv->primitiveType == PRIMITIVE_TYPE_DWORD)
+  {
+    DWordVariable* dwrd = (DWordVariable*)bpv;
+
+    long conv = (long)v;
+    bool ret = false;
+
+    if (dwrd->value != conv)
+    {
+      dwrd->value = conv;
+      ret = true;
+    }
+
+    return ret;
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_QWORD)
+  {
+    QWordVariable* qwrd = (QWordVariable*)bpv;
+
+    long long conv = (long long)v;
+    bool ret = false;
+
+    if (qwrd->value != conv)
+    {
+      qwrd->value = conv;
+      ret = true;
+    }
+
+    return ret;
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_DWORD)
+  {
+    UnsignedDWordVariable* udwrd = (UnsignedDWordVariable*)bpv;
+
+    unsigned long conv = (unsigned long)v;
+    bool ret = false;
+
+    if (udwrd->value != conv)
+    {
+      udwrd->value = conv;
+      ret = true;
+    }
+
+    return ret;
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_QWORD)
+  {
+    UnsignedQWordVariable* uqwrd = (UnsignedQWordVariable*)bpv;
+
+    unsigned long long conv = (unsigned long long)v;
+    bool ret = false;
+
+    if (uqwrd->value != conv)
+    {
+      uqwrd->value = conv;
+      ret = true;
+    }
+
+    return ret;
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_SINGLE)
+  {
+    SingleVariable* sngl = (SingleVariable*)bpv;
+
+    if (sngl->value != v)
+    {
+      sngl->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_DOUBLE)
+  {
+    DoubleVariable* dbl = (DoubleVariable*)bpv;
+
+    if (dbl->value != v)
+    {
+      dbl->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+#if TARGET_PLATFORM == PLATFORM_WINDOWS
+  sprintf_s(errorMessage, sizeof(errorMessage),
+#else
+  sprintf(errorMessage,
+#endif
+    "Received Single cannot be assigned to %s of type %s",
+    bpv->name.c_str(), bpv->getPrintableTypeName());
+
+  logger->error(errorMessage);
+
   return false;
 }
 
