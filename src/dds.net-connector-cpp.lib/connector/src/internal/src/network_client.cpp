084242c737f71e01730f133370eecc629449a035
usama <mahmoodify@gmail.com>
2023-04-07 09:15:47 +0500

    initializing queues

--- src/dds.net-connector-cpp.lib/connector/src/internal/src/network_client.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/internal/src/network_client.cpp
@@ -2,13 +2,17 @@
 
 #include "src/internal/inc/sync_queue_reader.h"
 #include "src/internal/inc/sync_queue_writer.h"
+#include "src/internal/inc/sync_queue.h"
 
 
 using namespace dds::net::connector::_internal;
 
 
 dds::net::connector::_internal::
-  NetworkClient::NetworkClient(BufferManager* bufferManager)
+  NetworkClient::NetworkClient(
+    BufferManager* bufferManager,
+    int dataToServerQueueSize,
+    int dataFromServerQueueSize)
 {
   this->bufferManager = bufferManager;
 
@@ -17,6 +21,9 @@ dds::net::connector::_internal::
 
   this->onDisconnected = nullptr;
   this->onDisconnectedObj = nullptr;
+
+  this->dataToServerQueue = new SyncQueue<PacketToServer*>(dataToServerQueueSize);
+  this->dataFromServerQueue = new SyncQueue<PacketFromServer*>(dataFromServerQueueSize);
 }
 
 SyncQueueReader<PacketFromServer*>*
