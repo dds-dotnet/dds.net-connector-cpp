0ae517c712dabb751d0254b4962ba15b1b9438d2
usama <mahmoodify@gmail.com>
2023-04-07 06:56:33 +0500

    impl unsigned byte assignment

--- src/dds.net-connector-cpp.lib/connector/src/ddsconnector-primassign.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/ddsconnector-primassign.cpp
@@ -411,6 +411,143 @@ bool
   dds::net::connector::
   DdsConnector::updatePrimitiveVariableWithUnsignedByte(BasePrimitive* bpv, unsigned char v)
 {
+  if (bpv->primitiveType == PRIMITIVE_TYPE_WORD)
+  {
+    WordVariable* wrd = (WordVariable*)bpv;
+
+    if (wrd->value != v)
+    {
+      wrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_DWORD)
+  {
+    DWordVariable* dwrd = (DWordVariable*)bpv;
+
+    if (dwrd->value != v)
+    {
+      dwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_QWORD)
+  {
+    QWordVariable* qwrd = (QWordVariable*)bpv;
+
+    if (qwrd->value != v)
+    {
+      qwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_BYTE)
+  {
+    UnsignedByteVariable* ubt = (UnsignedByteVariable*)bpv;
+
+    if (ubt->value != v)
+    {
+      ubt->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_WORD)
+  {
+    UnsignedWordVariable* uwrd = (UnsignedWordVariable*)bpv;
+
+    if (uwrd->value != v)
+    {
+      uwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_DWORD)
+  {
+    UnsignedDWordVariable* udwrd = (UnsignedDWordVariable*)bpv;
+
+    if (udwrd->value != v)
+    {
+      udwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_QWORD)
+  {
+    UnsignedQWordVariable* uqwrd = (UnsignedQWordVariable*)bpv;
+
+    if (uqwrd->value != v)
+    {
+      uqwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_SINGLE)
+  {
+    SingleVariable* sngl = (SingleVariable*)bpv;
+
+    if (sngl->value != v)
+    {
+      sngl->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_DOUBLE)
+  {
+    DoubleVariable* dbl = (DoubleVariable*)bpv;
+
+    if (dbl->value != v)
+    {
+      dbl->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+#if TARGET_PLATFORM == PLATFORM_WINDOWS
+  sprintf_s(errorMessage, sizeof(errorMessage),
+#else
+  sprintf(errorMessage,
+#endif
+    "Received Unsigned Byte cannot be assigned to %s of type %s",
+    bpv->name.c_str(), bpv->getPrintableTypeName());
+
+  logger->error(errorMessage);
+
   return false;
 }
 
