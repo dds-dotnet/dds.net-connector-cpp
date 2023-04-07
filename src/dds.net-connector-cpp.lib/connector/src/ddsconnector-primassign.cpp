7034bdf9706e54b9c658bbdc1515c54969b51d88
usama <mahmoodify@gmail.com>
2023-04-07 07:40:39 +0500

    impl unsigned dword assignment

--- src/dds.net-connector-cpp.lib/connector/src/ddsconnector-primassign.cpp
+++ src/dds.net-connector-cpp.lib/connector/src/ddsconnector-primassign.cpp
@@ -671,6 +671,87 @@ bool
   dds::net::connector::
   DdsConnector::updatePrimitiveVariableWithUnsignedDWord(BasePrimitive* bpv, unsigned long v)
 {
+  if (bpv->primitiveType == PRIMITIVE_TYPE_QWORD)
+  {
+    QWordVariable* qwrd = (QWordVariable*)bpv;
+
+    if (qwrd->value != v)
+    {
+      qwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_DWORD)
+  {
+    UnsignedDWordVariable* udwrd = (UnsignedDWordVariable*)bpv;
+
+    if (udwrd->value != v)
+    {
+      udwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_UNSIGNED_QWORD)
+  {
+    UnsignedQWordVariable* uqwrd = (UnsignedQWordVariable*)bpv;
+
+    if (uqwrd->value != v)
+    {
+      uqwrd->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_SINGLE)
+  {
+    SingleVariable* sngl = (SingleVariable*)bpv;
+
+    if (sngl->value != v)
+    {
+      sngl->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else if (bpv->primitiveType == PRIMITIVE_TYPE_DOUBLE)
+  {
+    DoubleVariable* dbl = (DoubleVariable*)bpv;
+
+    if (dbl->value != v)
+    {
+      dbl->value = v;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+#if TARGET_PLATFORM == PLATFORM_WINDOWS
+  sprintf_s(errorMessage, sizeof(errorMessage),
+#else
+  sprintf(errorMessage,
+#endif
+    "Received Unsigned DWord cannot be assigned to %s of type %s",
+    bpv->name.c_str(), bpv->getPrintableTypeName());
+
+  logger->error(errorMessage);
+
   return false;
 }
 
